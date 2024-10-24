b5d4f5056a848b732328fa7a1e06b903f462b24e
Jules-wild <julesmarcilloux11@gmail.com>
2024-10-24 14:34:16 +0200

    Pokédex ex 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
+
 function App() {
+	const pokemonList = [
+		{
+			name: "rondoudou",
+			imgSrc: "800px-Rondoudou-RFVF.png",
+		},
+		{
+			name: "mew",
+		},
+	];
 	return (
 		<div>
-			<PokemonCard />
+			<PokemonCard pokemon={pokemonList[0]} />
 		</div>
 	);
 }
